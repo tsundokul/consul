bcabcde1e050da041a1a4d462dacab27122134a7
tsundokul <42388638+tsundokul@users.noreply.github.com>
2021-10-23 17:07:26 +0300

    Restrict languages to RO and EN (#32)

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,7 +1,9 @@
 module Consul
   class Application < Rails::Application
-    config.i18n.default_locale = :en
     config.deploy = config_for(:deploy)
     config.action_view.cache_template_loading = false
+
+    config.i18n.default_locale = :ro
+    config.i18n.available_locales = %w[ro en]
   end
 end
